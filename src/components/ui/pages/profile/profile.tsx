6ecc783aed75cbd9c75f2aa3a93167e37e50bcbc
Raul Gasanov <raull.gasanovv@gmail.com>
2024-12-30 17:17:24 +0300

    Adding tests

--- src/components/ui/pages/profile/profile.tsx
+++ src/components/ui/pages/profile/profile.tsx
@@ -35,7 +35,8 @@ export const ProfileUI: FC<ProfileUIProps> = ({
             errorText={''}
             size={'default'}
             icon={'EditIcon'}
-          />
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}          />
         </div>
         <div className='pb-6'>
           <Input
